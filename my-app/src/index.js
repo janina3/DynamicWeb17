3ab97e08023dfb869e9bcc561a6999b0335cb0b9
janina3 <janina3@icloud.com>
2017-04-17 18:46:22 -0400

    first commit

---
+++ my-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import ConvertTemp from './ConvertTemp'
+import './index.css';
+
+ReactDOM.render(
+  <ConvertTemp />,
+  document.getElementById('root')
+);
